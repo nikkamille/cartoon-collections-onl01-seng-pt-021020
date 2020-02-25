6532cd3e89430a705d2c75867ae63aaa72efeab2
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-25 06:55:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(name)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|planeteer_calls| planeteer_calls.upcase + "!"}
+  planeteer_calls.map {|planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
