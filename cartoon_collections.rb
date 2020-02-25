b71c4f89f67cdc9f4cd87a10878c7646ac22b1e8
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-25 07:05:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(name) 
   name.each_with_index {|name, index| puts "#{index + 1}. " "#{name}"}
 end
@@ -8,6 +10,7 @@ end
 
 def long_planeteer_calls(words)
   words.include?(words.count > 4)
+  binding.pry
 end
 
 def find_the_cheese# code an argument here
