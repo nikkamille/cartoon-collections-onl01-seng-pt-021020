55b78fbe32a70a82375765562d4dce64e6115899
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-25 07:14:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def long_planeteer_calls(words)
   # words.include?(words.length > 4)
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
