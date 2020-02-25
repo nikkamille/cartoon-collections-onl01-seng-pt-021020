c5c51756860a73ee0a73abf07925952e15d5f506
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-25 07:09:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.each.include? {|word| word
+  # words.each.include? {|word| word
+  words.include?(words.length)
 end
 
 def find_the_cheese# code an argument here
