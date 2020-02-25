324b1d937d31ec58b6e0989538f365a69580381d
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-25 06:50:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(name) # code an argument here
-  name.each_with_index {|name, index| puts "#{index}" "#{name}"}
+  name.each_with_index {|name, index| puts "#{index + 1}. " "#{name}"}
 end
 
 def summon_captain_planet# code an argument here
